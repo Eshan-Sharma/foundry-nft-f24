3e7fe56f16967cbeeab199cdc5b8c7ef6ee411c6
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-06-07 08:22:23 +0530

    testing BasicNFT

--- test/BasicNFT.t.sol
+++ test/BasicNFT.t.sol
@@ -2,5 +2,31 @@
 pragma solidity ^0.8.18;
 
 import {Test} from "forge-std/Test.sol";
+import {BasicNFT} from "../src/BasicNFT.sol";
+import {DeployBasicNFT} from "../script/DeployBasicNFT.s.sol";
 
-contract BasicNFTTest is Test {}
+contract BasicNFTTest is Test {
+    DeployBasicNFT public deployer;
+    BasicNFT public nft;
+    address USER = makeAddr("user");
+    string public constant DESIGNER_PUG =
+        "ipfs://QmQNfTA2QxqNcJMr8FDMuhHJvv7eac2R836wpyQQsxffsx?filename=designer-pug.json";
+
+    function setUp() public {
+        deployer = new DeployBasicNFT();
+        nft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Dogie";
+        string memory actualName = nft.name();
+        assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        nft.mintNft(DESIGNER_PUG);
+        assert(nft.balanceOf(USER) == 1);
+        assert(keccak256(abi.encodePacked(DESIGNER_PUG)) == keccak256(abi.encodePacked(nft.tokenURI(0))));
+    }
+}
