b497ba1f7f9f6549760a72a17914025df4bcc212
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-06-07 13:00:47 +0530

    change svg based on Mood

--- src/MoodNFT.sol
+++ src/MoodNFT.sol
@@ -2,22 +2,84 @@
 pragma solidity ^0.8.18;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNFT is ERC721 {
     uint256 private s_tokenCounter;
-    string private s_sadSvg;
-    string private s_happySvg;
+    string private s_sadSvgImageUri;
+    string private s_happySvgImageUri;
 
-    constructor(string memory sadSvg, string memory happySvg) ERC721("MoodNFT", "MN") {
+    enum NFTState {
+        HAPPY,
+        SAD
+    }
+
+    mapping(uint256 => NFTState) private s_tokenIdToState;
+
+    error MoodNft__CantFlipMoodIfNotOwner();
+
+    constructor(string memory sadSvgImageUri, string memory happySvgImageUri) ERC721("MoodNFT", "MN") {
         s_tokenCounter = 0;
-        s_sadSvg = sadSvg;
-        s_happySvg = happySvg;
+        s_sadSvgImageUri = sadSvgImageUri;
+        s_happySvgImageUri = happySvgImageUri;
     }
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToState[s_tokenCounter] = NFTState.HAPPY;
         s_tokenCounter++;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {}
+    function flipMood(uint256 tokenId) public {
+        if (getApproved(tokenId) != msg.sender && ownerOf(tokenId) != msg.sender) {
+            revert MoodNft__CantFlipMoodIfNotOwner();
+        }
+
+        if (s_tokenIdToState[tokenId] == NFTState.HAPPY) {
+            s_tokenIdToState[tokenId] = NFTState.SAD;
+        } else {
+            s_tokenIdToState[tokenId] = NFTState.HAPPY;
+        }
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        string memory imageURI = s_happySvgImageUri;
+
+        if (s_tokenIdToState[tokenId] == NFTState.SAD) {
+            imageURI = s_sadSvgImageUri;
+        }
+        return string(
+            abi.encodePacked(
+                _baseURI(),
+                Base64.encode(
+                    bytes( // bytes casting actually unnecessary as 'abi.encodePacked()' returns a bytes
+                        abi.encodePacked(
+                            '{"name":"',
+                            name(), // You can add whatever name here
+                            '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                            '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                            imageURI,
+                            '"}'
+                        )
+                    )
+                )
+            )
+        );
+    }
+
+    function getHappySVG() public view returns (string memory) {
+        return s_happySvgImageUri;
+    }
+
+    function getSadSVG() public view returns (string memory) {
+        return s_sadSvgImageUri;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
 }
