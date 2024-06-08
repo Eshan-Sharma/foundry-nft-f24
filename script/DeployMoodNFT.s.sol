2372250298d7625d0c5555e4596de00cd2d1b123
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-06-08 06:54:58 +0530

    DeployMoodNFT and testing it

---
+++ script/DeployMoodNFT.s.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MoodNFT} from "../src/MoodNFT.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DeployMoodNFT is Script {
+    function run() external returns (MoodNFT) {
+        // MoodNFT nft = new MoodNFT();
+        // return nft;
+    }
+
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        string memory baseURI = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(baseURI, svgBase64Encoded));
+    }
+}
